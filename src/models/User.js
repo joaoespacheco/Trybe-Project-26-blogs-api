a3aa44b9639d10d6263b7c065a42146b8d206493
joao emanuel soares pacheco <joaoe.pacheco@gmail.com>
2022-10-26 16:11:27 -0300

    Requisito 02 - Criação model User - Concluído

---
+++ src/models/User.js
@@ -0,0 +1,25 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    tableName: 'users',
+    underscored: true,
+    timestamps: false,
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, {
+      foreignKey: 'user_id',
+      as: 'blog_posts',
+    })
+  }
+
+  return User;
+}
