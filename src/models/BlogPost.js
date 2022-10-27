978692a30651f9615264d3d5cf628fec7683f6a5
joao emanuel soares pacheco <joaoe.pacheco@gmail.com>
2022-10-27 19:38:47 -0300

    Requisito 10 - Inserção da model BlogPost - Concluído

---
+++ src/models/BlogPost.js
@@ -0,0 +1,30 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: { 
+      type: DataTypes.INTEGER,
+      foreignKey: true,
+    },
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+  }, {
+    tableName: 'blog_posts',
+    underscored: true,
+    createdAt: 'published',
+    updatedAt: 'updated',
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, {
+      foreignKey: "user_id",
+      as: "users",
+    });
+  };
+
+  return BlogPost;
+}
